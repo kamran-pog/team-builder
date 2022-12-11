9522c4608841be73c6dd122b40482dce48ee528c
kamran-pog <kamran.k0807@gmail.com>
2022-12-11 10:44:09 -0800

    MVP updated

--- src/App.js
+++ src/App.js
@@ -4,32 +4,32 @@ import Form from './components/Form'
 
 function App() {
   const [members, setMembers] = useState([]);
-  const [values, setValues] = useState({name: '', email:'', role:''});      
- 
+  const [values, setValues] = useState({ name: '', email: '', role: '' });
+
   const onSubmit = () => {
-    setMembers([values, ...members]);  
+    setMembers([values, ...members]);
   }
-   
-  const onChange = (name, value) =>  {
-    setValues({ ...values, [name]: value})
+
+  const onChange = (name, value) => {
+    setValues({ ...values, [name]: value })
   }
 
   return (
     <div className="App">
-       <Form
-    values={values}
-    change={onChange}
-    submit={onSubmit}
-    />
-    {members.map(member => {
-      return(
-        <div>
-          {member.name}, {member.email}, {member.role}
-        </div>
-      )
-    })}
+      <Form
+        values={values}
+        change={onChange}
+        submit={onSubmit}
+      />
+      {members.map(member => {
+        return (
+          <div>
+            {member.name}, {member.email}, {member.role}
+          </div>
+        )
+      })}
     </div>
   );
 }
- 
+
 export default App;
