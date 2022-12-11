bc73e7498804ace84ced4861e6d319592c512c8e
kamran-pog <kamran.k0807@gmail.com>
2022-12-11 10:41:49 -0800

    MVP updated

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css';
+import Form from './components/Form'
+
+function App() {
+  const [members, setMembers] = useState([]);
+  const [values, setValues] = useState({name: '', email:'', role:''});      
+ 
+  const onSubmit = () => {
+    setMembers([values, ...members]);  
+  }
+   
+  const onChange = (name, value) =>  {
+    setValues({ ...values, [name]: value})
+  }
+
+  return (
+    <div className="App">
+       <Form
+    values={values}
+    change={onChange}
+    submit={onSubmit}
+    />
+    {members.map(member => {
+      return(
+        <div>
+          {member.name}, {member.email}, {member.role}
+        </div>
+      )
+    })}
+    </div>
+  );
+}
+ 
+export default App;
